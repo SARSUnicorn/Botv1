5247d8dc23a545e43f609b26b5c3945b9f81de4f
SARSUnicorn <121972651+SARSUnicorn@users.noreply.github.com>
2023-07-03 14:55:38 +0200

    updatre

---
+++ responses.py
@@ -0,0 +1,14 @@
+﻿import random
+
+def handle_response(message) -> str:
+	p_messgae = message.lower()
+
+	if p_message == 'kim jest przelozony':
+		return 'Przełożonym jest żołnierz lub osoba nie będąca żołnierzem, któremu na mocy przepisów prawa, rozkazu, polecenia lub decyzji podporządkowano żołnierza (podwładnego), uprawniona do wydawania rozkazów, poleceń służbowych podwładnym żołnierzom lub osobom nie będącym żołnierzami i kierowania ich czynnościami służbowymi.'
+
+	if p_message == 'roll':
+		return str(random.randint(1, 6))
+
+	if p_messgae == 'bottom':
+		return "`There is a reason why he is a bottom`"
+	
